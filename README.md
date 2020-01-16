a4d1a82c4bdadd6f381a967f4df59645a3d18b65
Peter Graham <pwgraham91@gmail.com>
2020-01-15 22:40:51 -0600

    first

---
+++ README.md
@@ -0,0 +1,12 @@
+# Upgrade Gradle Dependencies
+
+This is a script which will search a build.gradle file and determine which dependencies are out of date.
+
+## Usage
+Install dependencies in requirements.txt
+
+Add your build.gradle file to the root of this project
+
+`python main.py`
+
+The script will print out which dependencies need to be upgraded.
---
+++ main.py
@@ -0,0 +1,32 @@
+import json
+import re
+
+import requests
+from packaging import version
+
+
+def run():
+    with open('build.gradle') as file:
+        lines = file.read()
+        regex_results = re.findall(r"[i|I]mplementation [\"|']([a-zA-Z\.\-\d]+):([a-zA-Z\.\-\d]+):([\d\.\-\w]+)[\"|']", lines)
+        for dependency in regex_results:
+            latest_version = get_latest_version(dependency[0], dependency[1])
+            if latest_version:
+                current_version = dependency[2]
+                should_upgrade = version.parse(latest_version) > version.parse(current_version)
+                if should_upgrade:
+                    print("{}:{} should upgrade from {} to {}".format(dependency[0], dependency[1], current_version, latest_version))
+
+
+def get_latest_version(part_1, part_2):
+    result = requests.get("https://search.maven.org/solrsearch/select?q={}&start=0&rows=2".format("{}%20AND%20a:{}".format(part_1, part_2)))
+    jsonified_result = json.loads(result.text)
+    docs = jsonified_result["response"]["docs"]
+    if len(docs) > 0:
+        return docs[0]["latestVersion"]
+    else:
+        print("no result for {}:{}".format(part_1, part_2))
+
+
+if __name__ == "__main__":
+    run()
